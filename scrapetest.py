1d824d4496095c2e4a6fb992ca46d4f1fd3d65af
Carlos Moreno <omorenodovale@gmail.com>
2020-02-12 11:05:05 -0500

    Refactoring scrapetest.py

--- scrapetest.py
+++ scrapetest.py
@@ -1,7 +1,23 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 
 
-html = urlopen("http://www.pythonscraping.com/exercises/exercise1.html")
-bsObj = BeautifulSoup(html.read())
-print(bsObj.h1)
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bsObj = BeautifulSoup(html.read())
+        title = bsObj.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+
+title = getTitle("http://pythonscraping.com/exercises/exercise1.html")
+if title == None:
+    print("Title could not be found")
+else:
+    print(title)
