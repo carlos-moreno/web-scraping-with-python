2f504c99404d537a60d13f211f4b75472ebdc56f
Carlos Moreno <omorenodovale@gmail.com>
2020-02-12 10:58:18 -0500

    Change scrapetest.py

--- scrapetest.py
+++ scrapetest.py
@@ -1,5 +1,7 @@
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
 
-html = urlopen("http://pythonscraping.com/pages/page1.html")
-print(html.read())
+html = urlopen("http://www.pythonscraping.com/exercises/exercise1.html")
+bsObj = BeautifulSoup(html.read())
+print(bsObj.h1)
